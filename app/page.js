06dab38c2f3339f6e8c6996c0ed745349b0eb5c7
Abhishek Singh <abhinav210702@gmail.com>
2024-04-04 20:55:26 +0530

    Added Locomotive Scroll

--- app/page.js
+++ app/page.js
@@ -1,13 +1,25 @@
-
+"use client"
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import { useEffect } from "react";
+import About from "@/components/About";
 export default function Home() {
+  useEffect( () => {
+    (
+      async () => {
+          const LocomotiveScroll = (await import('locomotive-scroll')).default
+          const locomotiveScroll = new LocomotiveScroll();
+      }
+    )()
+  }, [])
+
   return (
-    <>
+    <div>
       <Navbar />
       <main>
         <Hero />
+        <About />
       </main>
-    </>
+    </div>
   );
 }
