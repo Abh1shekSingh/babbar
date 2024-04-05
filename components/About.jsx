baeaacf5a1ff35ff17cfdc67729033ccbe3317a5
Abhishek Singh <abhinav210702@gmail.com>
2024-04-05 11:15:24 +0530

    Letter by Letter Animation

--- components/About.jsx
+++ components/About.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import { Saira } from 'next/font/google'
+const SairaF = Saira({subsets: ['latin'], weight: ['700']})
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 const About = () => {
+
   return (
-    <div>About</div>
+    <div className='h-[100vh]'>
+      <div className='ml-[5em]'>
+        <span className='block overflow-hidden'>
+          <span className={`block ${SairaF.className} uppercase text-[#3A3733] text-8xl`}>A little bit</span>
+        </span>
+        <span className='block overflow-hidden '>
+          <span className={`block ${SairaF.className} uppercase text-[#3A3733] ml-5 text-8xl`}>About me.</span>
+        </span>
+      </div>
+    </div>
   )
 }
 
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -9,34 +9,59 @@ import gsap from "gsap"
 import {useGSAP} from "@gsap/react";
 const Hero = () => {
 
+    const greetWord = [...("Hi There, I'm")]
+    const nameWord = [...("Abhishek Babbar.")]
+
+
     useGSAP(() => {
         gsap.from("#greeting", 
-            {y: 150, duration: 1.5, ease: "expo.out"}
+            {y: 150, duration: 1.5, ease: "expo.out", stagger: {amount: 0.8}}
         )
         gsap.from("#name", 
-            {y: 150, duration: 1.5,delay:0.2 ,ease: "expo.out"}
+            {y: 150, duration: 1.5,delay:0.2 ,ease: "expo.out", stagger: {amount: 0.8}}
         )
         gsap.from("#subtext", 
             {y: 100, opacity:0, duration: 1.5,delay:0.8 ,ease: "expo.out"}
         )
         gsap.from("#hero_image", 
-            {scale: 0, opacity:0, duration: 1,ease: "expo.out"}
+            {scale: 0, opacity:0, duration: 4,ease: "expo.out"}
         )
     }, [])
 
   return (
-    <div className='h-[100vh] w-full'>
+    <div className='h-[90vh] w-full'>
         <div className='absolute z-[0] w-[100%] sm:w-[50%] top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]'>
             <Image id='hero_image'  src={heroImage} alt='hero'/>
         </div>
         <div className='pl-[1em] absolute bottom-12 sm:bottom-0 sm:relative sm:flex sm:flex-col sm:justify-center sm:pt-[5em] sm:items-center'>
             <div className={`text-[#3A3733] uppercase font-bold text-4xl xl:text-9xl lg:text-7xl sm:text-5xl ${SairaF.className}`}>
-                <span className='block overflow-hidden'>
-                    <span id='greeting' className='block tracking-tight '>Hi There, I&apos;m</span>
-                </span>
-                <span className='block overflow-hidden'>
-                    <span id='name' className='sm:ml-3 block tracking-tight'>Abhishek Babbar.</span>
-                </span>
+                <div className='flex'>
+                    {greetWord.map((word, index) => {
+                        if(word !== ' ') {
+                            return <span className='block overflow-hidden'>
+                                <span id='greeting' className='flex block tracking-tight'>{word}</span>
+                            </span>
+                        }else {
+                            return <span className='ml-3 xl:ml-7 lg:ml-5 sm:ml-5 block overflow-hidden'>
+                                <span id='greeting' className='flex block tracking-tight'>{word}</span>
+                            </span>
+                        }
+                    })}
+                </div>
+                <div className='flex'>
+                    {nameWord.map((word, index) => {
+                        if(word !== ' ') {
+                            return <span className='block overflow-hidden'>
+                                <span id='greeting' className='flex block tracking-tight'>{word}</span>
+                            </span>
+                        }else {
+                            return <span className='ml-3 xl:ml-7 lg:ml-5 sm:ml-5 block overflow-hidden'>
+                                <span id='greeting' className='flex block tracking-tight'>{word}</span>
+                            </span>
+                        }
+                    })}
+                </div>
+                
             </div>
             <div className='text-[#777777] sm:w-[35%] sm:text-center font-medium text-sm xl:text-xl lg:text-lg sm:text-sm mt-2 sm:mt-7'>
                 <p id='subtext' className={`${BarlowF.className}`}>A software developer engineer II @ Urban company & developer who think ahead of the curve.</p>
