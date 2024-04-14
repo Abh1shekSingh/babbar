0296220030d5c91a266eac39227570f600f6fc41
Abhishek Singh <abhinav210702@gmail.com>
2024-04-14 14:08:45 +0530

    Meta data

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Abhishek Babbar — Software Engineer",
-  description: "I'm a software engineer based in India. I specialize in building (and occasionally designing) exceptional digital experiences. Currently, I'm focused on building accessible, human-centered products at Upstatement.",
+  description: "Abhishek Babbar is a software engineer based in India. He started his journey as an Intern with Digital Register and later switched to 364 Labs, Currently working with Urban Company, a startup based in Delhi, as a software engineer 2.",
 };
 
 export default function RootLayout({ children }) {
